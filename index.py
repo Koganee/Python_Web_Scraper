8c89ef701abd36e71c25dcddb3b0845fed952316
Aaron Murphy <126209301+Oakamer@users.noreply.github.com>
2024-07-27 20:03:03 +0100

    Update index.py

--- index.py
+++ index.py
@@ -1,35 +1,47 @@
 from tkinter import *
 from tkinter import ttk
-from tkinter import Tk
 import subprocess
 import sys
 import os
+import pymongo
 
+MONGODB_SERVER = "localhost"
+MONGODB_PORT = 27017
+MONGODB_DB = "stackoverflow"
+MONGODB_COLLECTION = "questions"
 
-#def retrieve_input():
-#    value = str(inputWord.get())
-#    print("Input value:", value)
-#    run_scrapy()
+def getWord():
+    searchValue = inputWord.get()
+    searchDatabase(searchValue)
 
-def run_scrapy():
+def searchDatabase(searchValue):
+    count = 0
     try:
-        os.chdir('C:\\Users\\Aaron\\Web_Scraper\\stack') 
-        # Run the Scrapy crawl command
-        result = subprocess.run(
-            ["scrapy", "crawl", "stack", "-a", f"value={inputWord.get()}"],
-            check=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True
-        )
-        # Print standard output and error
-        #print("Scrapy command executed successfully.")
-        #print("Standard Output:\n", result.stdout)
-        #print("Standard Error:\n", result.stderr)
-    except subprocess.CalledProcessError as e:
-        print(f"Error occurred: {e}")
-        #print("Standard Output:\n", e.stdout)
-        print("Standard Error:\n", e.stderr)
+        # Connect to MongoDB
+        client = pymongo.MongoClient(MONGODB_SERVER, MONGODB_PORT)
+        db = client[MONGODB_DB]
+        collection = db[MONGODB_COLLECTION]
+
+        # Query the database
+        query = {"title": {"$regex": searchValue, "$options": "i"}}  # Using text index for searching
+        results = collection.find(query)
+        array = [results]
+        lst = [1000000]
+        # Print results
+        for result in results:
+            title = result.get("title", "")
+            if searchValue.lower() in title.lower():
+                if result not in lst:
+                    print(result)
+                lst[count] = result
+            else:
+                print("Hi") 
+            count += 1       
+
+    finally:
+        # Close the connection
+        # print("Total questions matching search: " + count)
+        client.close()
 
 root = Tk()
 frm = ttk.Frame(root, padding=100)
@@ -40,11 +52,6 @@ ttk.Label(frm, text="Search Word Here:").grid(column=0, row=0)
 inputWord = ttk.Entry(frm)
 inputWord.grid(column=1, row=0)
 
-value = str(inputWord.get())
-
-ttk.Button(frm, text="Get Value", command=run_scrapy).grid(column=1, row=1)    
+ttk.Button(frm, text="Get Value", command=getWord).grid(column=1, row=1)    
 root.mainloop()
-
-
-
 
