bbb77a04d3286c38ea9cd3ade35d8bbd43a55b2f
Aaron Murphy <126209301+Oakamer@users.noreply.github.com>
2024-07-28 18:07:32 +0100

    Update index.py

--- index.py
+++ index.py
@@ -10,11 +10,12 @@ MONGODB_PORT = 27017
 MONGODB_DB = "stackoverflow"
 MONGODB_COLLECTION = "questions"
 
-def getWord():
+def getWord():                                                          #Function for getting word
     searchValue = inputWord.get()
     searchDatabase(searchValue)
+    findMostCommonWords(searchValue)
 
-def searchDatabase(searchValue):
+def searchDatabase(searchValue):                                        #Function for searching for specified word.
     count = 0
     try:
         # Connect to MongoDB
@@ -23,35 +24,64 @@ def searchDatabase(searchValue):
         collection = db[MONGODB_COLLECTION]
 
         # Query the database
-        query = {"title": {"$regex": searchValue, "$options": "i"}}  # Using text index for searching
+        query = {"title": {"$regex": searchValue, "$options": "i"}}     #Includes substrings.
         results = collection.find(query)
-        array = [results]
-        lst = [1000000]
+        lst = []
+
         # Print results
         for result in results:
             title = result.get("title", "")
-            if searchValue.lower() in title.lower():
-                if result not in lst:
+            url = result.get("url", "")                                 #Get URL
+            if searchValue.lower() in title.lower():                    #Check if searchValue is in title.
+                if url not in lst:                                      #Only prints if url is not already in list.
                     print(result)
-                lst[count] = result
+                lst.append(url)                                         #Add url to list.
             else:
-                print("Hi") 
-            count += 1       
+                print("Hi")     
 
     finally:
         # Close the connection
-        # print("Total questions matching search: " + count)
         client.close()
 
+def findMostCommonWords(searchValue):
+    try:
+        # Connect to MongoDB
+        client = pymongo.MongoClient(MONGODB_SERVER, MONGODB_PORT)
+        db = client[MONGODB_DB]
+        collection = db[MONGODB_COLLECTION]
+
+        word_counts = {}
+        listWords = []
+
+        # Query the database
+        
+        results = collection.find({})
+
+        for result in results:
+            title = result.get("title", "")
+            words = title.split()
+            for word in words:
+                if word not in listWords:
+                    listWords.append(word)
+                    word_counts[word] = 1
+                else:
+                    word_counts[word] += 1
+        for word, count in word_counts.items():
+            print(f"'{word}': {count}") 
+            #print(f"'{searchValue}': {count}")                 
+    finally:
+        # Close the connection
+        client.close()
+
+#UI---------------------------------------------------
 root = Tk()
 frm = ttk.Frame(root, padding=100)
 frm.grid()
 ttk.Label(frm, text="Search Word Here:").grid(column=0, row=0)
-#ttk.Button(frm, text="Search", command=run_scrapy).grid(column=2, row=0)
 
-inputWord = ttk.Entry(frm)
+inputWord = ttk.Entry(frm)                                              #Search Input Box.
 inputWord.grid(column=1, row=0)
 
 ttk.Button(frm, text="Get Value", command=getWord).grid(column=1, row=1)    
 root.mainloop()
-
+#UI---------------------------------------------------
